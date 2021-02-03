8781ba0a29daffa08e25f1120ae5e9537554537f
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-02-03 18:04:29 +0600

    first commit

---
+++ a.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class a  //problem 1335A
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+			int n=sc.nextInt()-1;
+			n=n/2;
+			System.out.println(n);
+		}
+	}
+}
